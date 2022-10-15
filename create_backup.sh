043c700f8efe0ce8cd99ef6b78b8eff915e52b2e
CrPO4 <32630390+CrPO4@users.noreply.github.com>
2022-10-15 21:00:26 +0300

    Update create_backup.sh

--- create_backup.sh
+++ create_backup.sh
@@ -2,7 +2,7 @@ hostname=$(hostname)
 date=$(date +"%Y-%m-%d")
 tar -czf easy-wireguard-server-$date-$hostname-backup.tar.gz /etc/wireguard 1>/dev/null 2>/dev/null
 if ! [ -f "easy-wireguard-server-$date-$hostname-backup.tar.gz" ]; then
-    echo "Backup created."
+    echo "easy-wireguard-server-$date-$hostname-backup.tar.gz: Backup created."
 else
     echo "Backup creation failed."
 fi
