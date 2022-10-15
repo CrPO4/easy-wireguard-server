f6a633f83ee381585918ac8b0bc96fbd2efa75e6
CrPO4 <32630390+CrPO4@users.noreply.github.com>
2022-10-15 21:18:39 +0300

    Update setup_server_from_backup.sh

--- setup_server_from_backup.sh
+++ setup_server_from_backup.sh
@@ -1,6 +1,6 @@
 mkdir temp
 cd temp
-tar -xvzf ../easywireguard*
+tar -xvzf ../easy-wireguard*
 mv -fv ./etc/wireguard/clients/* /etc/wireguard/clients
 rm -f ./etc/wireguard/clients
 mv -fv ./etc/wireguard/* /etc/wireguard
