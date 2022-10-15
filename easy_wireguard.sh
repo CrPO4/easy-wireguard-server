7753f62edff44735381fae2ef290ee2872c80d60
CrPO4 <32630390+CrPO4@users.noreply.github.com>
2022-10-15 19:59:18 +0300

    New mode: from backup

--- easy_wireguard.sh
+++ easy_wireguard.sh
@@ -11,34 +11,41 @@ if ! [ -d "easy_wireguard" ]; then
 fi
 cd easy_wireguard
 if ! [ -f "setup_server.sh" ]; then
-    curl -O https://raw.githubusercontent.com/bllizard22/easy-wireguard-server/main/setup_server.sh
+    curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/setup_server.sh
 	chmod +x setup_server.sh
 fi
+if ! [ -f "setup_server_from_backup.sh" ]; then
+    curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/setup_server_from_backup.sh
+	chmod +x setup_server_from_backup.sh
+fi
 if ! [ -f "add_client.sh" ]; then
-    curl -O https://raw.githubusercontent.com/bllizard22/easy-wireguard-server/main/add_client.sh
+    curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/add_client.sh
 	chmod +x add_client.sh
 fi
 if ! [ -f "remove_server.sh" ]; then
-	curl -O https://raw.githubusercontent.com/bllizard22/easy-wireguard-server/main/remove_server.sh
+	curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/remove_server.sh
 	chmod +x remove_server.sh
 fi
 cd ..
 
 echo -en "${GREEN}Choose the action:
-[1] - Setup server
-[2] - Add new client (peer)
-${RED}[3] - Remove server from this machine${GREEN}
+[1] Setup WireGuard server
+[2] Setup WireGuard server from backup
+[3] Add new client (peer)
+${RED}[4] Remove WireGuard server from this system${GREEN}
 
-[1/2/3]: ${NC}"
+[1/2/3/4]: ${NC}"
 read OPTION
 
 if [ $OPTION == "1" ]; then
 	./easy_wireguard/setup_server.sh
 elif [ $OPTION == "2" ]; then
-	./easy_wireguard/add_client.sh
+	./easy_wireguard/setup_server_from_backup.sh
 elif [ $OPTION == "3" ]; then
+	./easy_wireguard/add_client.sh
+elif [ $OPTION == "4" ]; then
 	./easy_wireguard/remove_server.sh
 	rm -r ./easy_wireguard
 else
-	echo -e "${PURPLE}Exit without actions${NC}"
+	echo -e "${PURPLE}Exit: the system was not modified.${NC}"
 fi
