83932aef7c2cbaabd29029d4bbbb184b00327a4f
CrPO4 <32630390+CrPO4@users.noreply.github.com>
2022-10-15 21:41:56 +0300

    Update easy_wireguard.sh

--- easy_wireguard.sh
+++ easy_wireguard.sh
@@ -18,13 +18,17 @@ if ! [ -f "setup_server_from_backup.sh" ]; then
     curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/setup_server_from_backup.sh
 	chmod +x setup_server_from_backup.sh
 fi
-if ! [ -f "create_backup.sh" ]; then
-    curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/create_backup.sh
-	chmod +x create_backup.sh
-fi
 if ! [ -f "add_client.sh" ]; then
     curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/add_client.sh
 	chmod +x add_client.sh
+fi
+if ! [ -f "show_qr.sh" ]; then
+    curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/show_qr.sh
+	chmod +x show_qr.sh
+fi
+if ! [ -f "create_backup.sh" ]; then
+    curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/create_backup.sh
+	chmod +x create_backup.sh
 fi
 if ! [ -f "remove_server.sh" ]; then
 	curl -O https://raw.githubusercontent.com/crpo4/easy-wireguard-server/main/remove_server.sh
@@ -36,10 +40,11 @@ echo -en "${GREEN}Choose the action:
 [1] Setup WireGuard server
 [2] Setup WireGuard server from backup
 [3] Add new client (peer)
-[4] Create configuration backup
-${RED}[5] Remove WireGuard server from this system${GREEN}
+[4] Show client (peer) QR
+[5] Create configuration backup
+${RED}[6] Remove WireGuard server from this system${GREEN}
 
-[1/2/3/4/5]: ${NC}"
+[1/2/3/4/5/6]: ${NC}"
 read OPTION
 
 if [ $OPTION == "1" ]; then
@@ -49,8 +54,10 @@ elif [ $OPTION == "2" ]; then
 elif [ $OPTION == "3" ]; then
 	./easy_wireguard/add_client.sh
 elif [ $OPTION == "4" ]; then
-	./easy_wireguard/create_backup.sh
+	./easy_wireguard/show_qr.sh
 elif [ $OPTION == "5" ]; then
+	./easy_wireguard/create_backup.sh
+elif [ $OPTION == "6" ]; then
 	./easy_wireguard/remove_server.sh
 	rm -r ./easy_wireguard
 else
